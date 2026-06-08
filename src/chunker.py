ba09ff28aa5a64e6b9e77c31959caf4bc889e3d1
GSatya147 <gvvsn147@gmail.com>
2026-06-08 22:13:11 +0100

    code: add sanity check method

--- src/chunker.py
+++ src/chunker.py
@@ -5,7 +5,7 @@ from sentence_transformers import SentenceTransformer
 class chunker:
     def __init__(self, FILE_PATH):
         self.CHUNK_SIZE = int(input("Enter chunk size please: "))
-        self.CHUNK_OVERLAP = 0.15 * self.CHUNK_SIZE
+        self.CHUNK_OVERLAP = int(0.15 * self.CHUNK_SIZE)
 
         self.FILE_PATH = FILE_PATH
 
@@ -21,38 +21,39 @@ class chunker:
             print(e)
 
     def corpus_chunker(self)-> list[dict]:
-        chunks = []
+        self.chunks = []
 
         i=0
         while i < len(self.tokenized_data):
-            chunks.append(self.tokenized_data[i: self.CHUNK_SIZE + i])
+            self.chunks.append(self.tokenized_data[i: self.CHUNK_SIZE + i])
             i+=self.CHUNK_SIZE - self.CHUNK_OVERLAP
 
         document_dict = dict()
         chunk_dict_list = []
 
-        for i in range(len(chunks)):
+        for i in range(len(self.chunks)):
             document_dict = {
-                "content": self.tokenizer.decode(chunks[i]),
+                "content": self.tokenizer.decode(self.chunks[i]),
                 "metadata": {
                     "source": "manual",
                     "document_name": os.path.basename(self.FILE_PATH),
                     "chunk_index": i,
-                    "token_count": len(chunks[i])
+                    "token_count": len(self.chunks[i])
                 }
             }
             chunk_dict_list.append(document_dict)
 
         return chunk_dict_list
 
-    def sanity_check(self, tokenized_data: list, chunks: list)-> None:
-        print(f"Total tokens: {len(tokenized_data)}")
-        print(f"Chunks: {len(chunks)}")
-        print(f"First chunk length: {len(chunks[0])}")
-        print(f"Pen-ultimate chunk length: {len(chunks[-2])}")
-        print(f"Ultimate chunk length: {len(chunks[-1])}")   
+    def sanity_check(self)-> None:
+        print(f"Total tokens: {len(self.tokenized_data)}")
+        print(f"Chunks: {len(self.chunks)}")
+        print(f"First chunk length: {len(self.chunks[0])}")
+        print(f"Pen-ultimate chunk length: {len(self.chunks[-2])}")
+        print(f"Ultimate chunk length: {len(self.chunks[-1])}")   
 
 if __name__=="__main__":
     obj = chunker("C:/Users/gvvsn/OneDrive/Desktop/Learning/Document Corpus/main_arcs.txt")
     result_list = obj.corpus_chunker()
     print(result_list)
+    obj.sanity_check()
