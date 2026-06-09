29f23804b24f665b9254e0909184c160c2206284
GSatya147 <gvvsn147@gmail.com>
2026-06-09 14:19:32 +0100

    code: add model inference optimisations

--- src/embedder.py
+++ src/embedder.py
@@ -6,21 +6,23 @@ from sentence_transformers import SentenceTransformer
 
 class VectorEmbedder:
     def __init__(self):
-        pass
+
+        try:
+            self.model = SentenceTransformer("voyageai/voyage-4-nano", trust_remote_code=True, truncate_dim=1024)
+        except Exception as e:
+            print(e)
 
     def corpus_embedder(self, chunk_size, file_path):
         chunker_obj = Chunker(chunk_size, file_path)
         self.chunker_result = chunker_obj.corpus_chunker()
 
         try:
-            model = SentenceTransformer("voyageai/voyage-4-nano", trust_remote_code=True, truncate_dim=1024)
-
             self.embeddings = []
             running_tokens = 0
             current_batch = []
             for chunk in self.chunker_result:
                 if running_tokens + chunk["metadata"]["token_count"] > 5000: 
-                    self.embeddings.extend(model.encode(current_batch))
+                    self.embeddings.extend(self.model.encode(current_batch))
 
                     running_tokens = 0
                     current_batch = []
@@ -29,7 +31,7 @@ class VectorEmbedder:
                 current_batch.append(chunk["content"])
             
             if current_batch:
-                self.embeddings.extend(model.encode(current_batch))
+                self.embeddings.extend(self.model.encode(current_batch))
 
         except Exception as e:
             print(e)
@@ -42,15 +44,16 @@ class VectorEmbedder:
 
             client = chromadb.PersistentClient(path="./chromadb")
 
-            client.get_or_create_collection(name="one_piece_main_arcs", metadata={"description": "all main story arcs in one piece present manga"})
-            client.add(
+            collection = client.get_or_create_collection(name="one_piece_main_arcs", metadata={"description": "all main story arcs in one piece present manga"})
+            
+            collection.add(
                 ids= [f"doc_{i}" for i in range(len(self.embeddings))],
                 documents= [chunk["content"] for chunk in self.chunker_result],
                 metadatas= [chunk["metadata"] for chunk in self.chunker_result],
                 embeddings= self.embeddings,
             )
 
-        except ExceptionGroup as e:
+        except Exception as e:
             print(e)
 
 if __name__=="__main__":
