51225a59880beb0451881beb5518c3e3e1c5ad38
GSatya147 <gvvsn147@gmail.com>
2026-06-09 16:55:48 +0100

    code: add env, dependencies, model

--- src/retriever.py
+++ src/retriever.py
@@ -1,5 +1,6 @@
 import chromadb
 from sentence_transformers import SentenceTransformer
+from groq import model
 
 class Retriever:
     def __init__(self, user_query):
@@ -24,6 +25,20 @@ class Retriever:
 
         return self.result
     
+    def generate_response(self):
+        self.corpus_retriever()
+
+        context = "/n/n".join()
+
+        sys_prompt = f"""
+            you are an one piece knowledge assistant, answer only on the basis of provided context:
+            <context>{context}</context>
+            if the given context is inadequate, just answer "It is out of the provided knowledge"
+        """
+
+
+        
+
 
 if __name__=="__main__":
     query = input(">> ")
