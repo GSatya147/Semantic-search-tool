48ea37739b578412bab2285ba44006673b8bb9e8
GSatya147 <gvvsn147@gmail.com>
2026-06-09 15:35:24 +0100

    code: add retriver core skeleton

---
+++ src/retriever.py
@@ -0,0 +1,36 @@
+import chromadb
+from sentence_transformers import SentenceTransformer
+
+class Retriever:
+    def __init__(self, user_query):
+        self.query = user_query
+
+        try:
+            self.model = SentenceTransformer("voyageai/voyage-4-nano", trust_remote_code=True ,truncate_dim=1024)
+            self.query_embeddings = self.model.encode(self.query)
+
+        except Exception as e:
+            print(e)
+
+    def corpus_retriever(self):
+        client = chromadb.PersistentClient(path="./chromadb")
+
+        collections = client.get_collection(name="one_piece_main_arcs")
+
+        self.result = collections.query(
+            query_embeddings=self.query_embeddings,
+            n_results=3
+        )
+
+        return self.result
+    
+
+if __name__=="__main__":
+    query = input(">> ")
+
+    obj = Retriever(query)
+    obj.corpus_retriever()
+
+    print(f"ids: {obj.result["ids"]}")
+    print(f"documents: {obj.result['documents']}")
+    print(f"distances: {obj.result["distances"]}")
