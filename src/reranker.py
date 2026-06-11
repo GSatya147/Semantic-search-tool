0504094fe973c84fd59acc3f874b7811eb44e692
GSatya147 <gvvsn147@gmail.com>
2026-06-11 12:46:04 +0100

    code: add reranker skeleton

--- src/reranker.py
+++ src/reranker.py
@@ -4,7 +4,29 @@ import chromadb
 from retriever import Retriever
 
 class Reranker:
-    pass
+    def __init__(self, user_query):
+        self.user_query = user_query
+
+        try:
+            self.model = CrossEncoder("")
+        except Exception as e:
+            print(e)
+
+    def rerank_results(self):
+        retreiver_obj = Retriever(self.user_query)
+        retriever_result = retreiver_obj.corpus_retriever()
+
+        self.sentences = []
+        for result in retriever_result:
+            batches = [self.user_query, result]
+            self.sentences.append(batches)
+
+        self.scores = self.model.predict(self.sentences)
+        
+        return self.scores
 
 if __name__=="__main__":
-    pass
+    user_query = input(">> ")
+    obj = Reranker(user_query)
+    result = obj.rerank_results()
+    print(result)
